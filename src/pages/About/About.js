f4ea69e2858a63561dcaf8c312e7f25fe6111258
Rabi-Mezan <ravimezan@gmail.com>
2022-01-24 23:55:22 +0600

    about us done

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -2,9 +2,35 @@ import React from 'react';
 import './About.css'
 import bgimage from '../../images/about1.jpg'
 
+
+
+
+
+const team = [
+
+    {
+        id: 1,
+        name: "Mr XXX",
+        img: 'https://this-person-does-not-exist.com/img/avatar-91942b18562b30754ed10dcbda98660d.jpg',
+        desination: "CEO"
+    },
+    {
+        id: 2,
+        name: "Mr YYY",
+        img: 'https://this-person-does-not-exist.com/img/avatar-1361dfe211b7226f8530d40645087c34.jpg',
+        desination: "Lead Engineer"
+    },
+    {
+        id: 3,
+        name: "Mr ZZZ",
+        img: 'https://this-person-does-not-exist.com/img/avatar-91942b18562b30754ed10dcbda98660d.jpg',
+        desination: "Property Analytics"
+    },
+]
+
 const About = () => {
     return (
-        <div>
+        <div >
             <div className='my-5'>
                 <h1 style={{ color: '#366e54' }} className='fs-1 fw-bold text-center '>ABOUT US</h1>
                 <p className='w-75 text-center mx-auto fw-light '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nam, sit quasi similique commodi adipisci tempore nostrum ad aut, odio autem impedit eveniet velit, quos nobis est laboriosam sint. Unde!</p>
@@ -17,34 +43,75 @@ const About = () => {
                     backgroundSize: "cover",
                     height: "100vh",
                     width: "100vw",
-                    color: "#f5f5f5"
+                    color: "/f5f5f5"
                 }}
                 className='paralax img-fluid'></div>
             <div className='row my-5'>
                 <section className='col-lg-6 col-12 '>
-                    <img className='img-fluid p-4' src="https://i.ibb.co/KrGtQ54/priscilla-du-preez-MXRk-GKLMf-Do-unsplash.jpg" alt="" />
+                    <img className='img-fluid my-5 p-4' src="https://i.ibb.co/KrGtQ54/priscilla-du-preez-MXRk-GKLMf-Do-unsplash.jpg" alt="" />
                 </section>
-                <section class="text-center about col-lg-6 col-12  ">
+                <section class="text-center d-flex align-items-baseline justify-content-center about col-lg-6 col-12  ">
                     <div class="container-fluid ">
-                        <div class="row p-4">
-                            <div class="col-lg-12 col-sm-6 col-ex-12 about-item wow lightSpeedIn" data-wow-offset="200" >
+                        <div class=" p-4">
+                            <div class=" about-item wow lightSpeedIn" data-wow-offset="200" >
                                 <div>
-                                    <span class="fa fa-group"></span>
+                                    <span class="fa fa-group p-2"></span>
                                 </div>
-                                <h4 className='text-dark fw-bold my-1'>OUR TEAM</h4>
+                                <h4 className='text-dark fw-bold my-3'>OUR TEAM</h4>
                                 <p class="lead fw-light">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+                                <div className='d-lg-flex gap-4'>
+                                    <div class="profile-card text-center bg-light p-4 my-5 w-lg-25 h-25 rounded-3">
+                                        <div class="profile-photo ">
+                                            <img className='w-25 h-25' src="https://this-person-does-not-exist.com/img/avatar-1361dfe211b7226f8530d40645087c34.jpg" class="img-fluid" alt='' />
+                                        </div>
+                                        <h3 class="pt-3 text-dark">Rajnish Kumar</h3>
+                                        <p class="text-secondary">CEO and Founder</p>
+                                        <div class="d-flex flex-row align-items-center justify-content-center">
+
+                                            <button type="button" class="btn btn-outline-dark btn-floating m-1">
+                                                <i class="fab fa-google"></i>
+                                            </button>
+                                            <button type="button" class="btn btn-outline-dark btn-floating m-1">
+                                                <i class="fab fa-facebook-f"></i>
+                                            </button>
+
+                                            <button type="button" class="btn btn-outline-dark btn-floating m-1">
+                                                <i class="fab fa-twitter"></i>
+                                            </button>
+
+
+                                        </div>
+                                    </div>
+                                    <div class="profile-card text-center bg-light p-4 my-5 w-lg-25 h-25 rounded-3">
+                                        <div class="profile-photo ">
+                                            <img className='w-25 h-25' src="https://this-person-does-not-exist.com/img/avatar-91942b18562b30754ed10dcbda98660d.jpg" class="img-fluid" alt='' />
+                                        </div>
+                                        <h3 class="pt-3 text-dark">Rajnish Kumar</h3>
+                                        <p class="text-secondary">Property Anlytics</p>
+                                        <div class="d-flex flex-row align-items-center justify-content-center">
+
+                                            <button type="button" class="btn btn-outline-dark btn-floating m-1">
+                                                <i class="fab fa-google"></i>
+                                            </button>
+                                            <button type="button" class="btn btn-outline-dark btn-floating m-1">
+                                                <i class="fab fa-facebook-f"></i>
+                                            </button>
+
+                                            <button type="button" class="btn btn-outline-dark btn-floating m-1">
+                                                <i class="fab fa-twitter"></i>
+                                            </button>
+
+
+                                        </div>
+                                    </div>
+                                </div>
+
+
+
+
                             </div>
-                            <div class="col-lg-12 col-sm-6 col-ex-12 about-item wow lightSpeedIn" data-wow-offset="200">
-                                <span class="fa fa-info"></span>
-                                <h2>Section 2 </h2>
-                                <p class="lead fw-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea vel tenetur laborum quam odit corporis dolorem mollitia cum reiciendis fugit nihil, illo architecto incidunt eius quibusdam suscipit nemo molestiae numquam. </p>
-                            </div>
+
                             <div class="clearfix visible-md-block visible-sm-block"></div>
-                            <div class="col-lg-12 col-sm-6 col-ex-12 about-item wow lightSpeedIn" data-wow-offset="200">
-                                <span class="fa fa-file"></span>
-                                <h2>Section 3</h2>
-                                <p class="lead fw-light">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
-                            </div>
 
                         </div>
 
